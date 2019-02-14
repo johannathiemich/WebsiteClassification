7104e8dfbfdf169ac4dbe8f44a2fcf2f8ba916a7
Alejandro Robles <alejandro.robles@altruisconsulting.com>
2019-02-13 21:00:06 -0800

    reading in data

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print('hello world')
+from pandas import DataFrame
+from json import load, loads
+
+
+data = []
+path = 'scrapedsites.json'
+
+with open(path) as f:
+    for line in f:
+        d = loads(line)
+        data.append(d)
+    dataFrame = DataFrame(data)
