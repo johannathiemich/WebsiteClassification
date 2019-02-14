d1f5f8fee6a0c6467848bfb9d8d578c6ebfb655d
Alejandro Robles <alejandro.robles@altruisconsulting.com>
2019-02-13 21:13:40 -0800

    Reading Pickled Data and Checking Sites not properly scraped

--- test.py
+++ test.py
@@ -10,3 +10,9 @@ with open(path) as f:
         d = loads(line)
         data.append(d)
     dataFrame = DataFrame(data)
+
+from pandas import read_pickle
+df = read_pickle('data.pkl')
+df.groupby('Category').count()
+
+len(df[df['Text'] == ""])
