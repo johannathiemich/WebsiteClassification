c08e5e54566c3c5bd1fe4dbc85f28284fd61a1fd
johannathiemich <johanna.thiemich@gmx.de>
2019-02-17 19:39:01 -0800

    some more tries to fix the error, not successful

--- data_cleaning.py
+++ data_cleaning.py
@@ -3,6 +3,7 @@
 from pandas import DataFrame
 from json import load, loads
 from pandas import read_pickle
+import numpy as np
 import nltk
 #only un-comment these for first use (need to download these!):
 #nltk.download('stopwords')
@@ -11,21 +12,26 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 data = []
-path = 'scrapedsites.json'
+json_path = 'scrapedsites.json'
+pkl_path = 'data.pkl'
 
 #AJ:
-with open(path) as f:
+with open(json_path) as f:
     for line in f:
         d = loads(line)
         data.append(d)
     dataFrame = DataFrame(data)
+    dataFrame.to_pickle(pkl_path)
+    np.savetxt(r'C:\Users\johan\OneDrive\Dokumente\Auslandssemester USA\Kurse\Machine Learning\project\WebsiteClassification\np.txt', dataFrame.values, fmt='%d')
 
-df = read_pickle('data.pkl')
-
+df = read_pickle(pkl_path)
+print("printing head:")
+print(df.head)
 #JT:
 #removing rows with empty text
 df = df[df['Text'] != ""]
-
+df = df[:5]
+print(df)
 #removing stopwords
 stop_words = set(stopwords.words('english'))
 for index, row in df.iterrows():
@@ -42,7 +48,8 @@ for index, row in df.iterrows():
     #print(word_tokens)
     #print(filtered_sentence)
 
-df.to_pickle("./data_cleaned.pkl")
-df.groupby('Category').count()
+print(df)
+df.to_pickle("data_cleaned.pkl")
+#df.groupby('Category').count()
 
 print(len(df[df['Text'] == ""]))
