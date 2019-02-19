b2830e42e62c5cbae51a3bb4d2f280caa41bf717
alejandro.robles <alejandro.robles@thetradedesk.com>
2019-02-19 08:17:18 -0800

    Train Model v=1

--- classify_websites.py
+++ classify_websites.py
@@ -6,3 +6,15 @@ import pickle
 df = read_pickle('files/data.pkl')
 
 model = fasttext.skipgram('test.txt', 'model')
+
+
+df[['Category', 'Text']].to_csv('data.txt', sep='\t', index=False, header=False)
+
+model = fasttext.skipgram('data.txt', 'model')
+
+
+classifier = fasttext.supervised('data.txt', 'model2')
+
+
+df[['Category', 'Text']].to_csv('data2.txt', sep='\t', index=False, header=True)
+
