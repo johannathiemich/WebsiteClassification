639875da97ec3644a264ddcdcfd2a23b548b6644
Alejandro Robles <alejandro.robles@thetradedesk.com>
2019-05-06 22:09:18 -0700

    keras model

--- transfer_learning.py
+++ transfer_learning.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import numpy as np
+import pandas as pd
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
 from keras.utils import to_categorical
@@ -8,6 +9,29 @@ from keras.layers import Dense, Input, GlobalMaxPooling1D
 from keras.layers import Conv1D, MaxPooling1D, Embedding
 from keras.models import Model
 from keras.initializers import Constant
+from sklearn.model_selection import train_test_split
+
+
+# https://github.com/keras-team/keras/blob/master/examples/pretrained_word_embeddings.py
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+BASE_DIR = os.getcwd()
+GLOVE_DIR = os.path.join(BASE_DIR, 'files/glove.6B')
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NUM_WORDS = 20000
+EMBEDDING_DIM = 100
+VALIDATION_SPLIT = 0.2
+
+
+
+csv_path_cleaned = 'files/data_cleaned.txt'
+
+df = pd.read_csv(csv_path_cleaned, header = None, names = ['Category', 'Text'], sep =' ')
+X = df['Text']
+y = df['Category']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state = 42)
 
 
 print('Indexing word vectors.')
@@ -18,3 +42,97 @@ with open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt')) as f:
         word, coefs = line.split(maxsplit=1)
         coefs = np.fromstring(coefs, 'f', sep=' ')
         embeddings_index[word] = coefs
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+
+# second, prepare text samples and their labels
+print('Processing text dataset')
+
+texts = []  # list of text samples
+labels = []  # list of label ids
+labels_index = {'__label__games&toys' : 0, '__label__sports' : 1, '__label__travel' : 2, '__label__food&drink' : 3}
+
+for index, row in df.iterrows():
+    texts.append(row['Text'])
+    labels.append(labels_index[row['Category']])
+
+
+
+
+print('Found %s texts.' % len(texts))
+
+# finally, vectorize the text samples into a 2D integer tensor
+tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+tokenizer.fit_on_texts(texts)
+sequences = tokenizer.texts_to_sequences(texts)
+
+word_index = tokenizer.word_index
+print('Found %s unique tokens.' % len(word_index))
+
+data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+
+labels = to_categorical(np.asarray(labels))
+print('Shape of data tensor:', data.shape)
+print('Shape of label tensor:', labels.shape)
+
+
+indices = np.arange(data.shape[0])
+np.random.shuffle(indices)
+data = data[indices]
+labels = labels[indices]
+num_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+x_train = data[:-num_validation_samples]
+y_train = labels[:-num_validation_samples]
+x_val = data[-num_validation_samples:]
+y_val = labels[-num_validation_samples:]
+
+print('Preparing embedding matrix.')
+
+
+
+# prepare embedding matrix
+num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+for word, i in word_index.items():
+    if i > MAX_NUM_WORDS:
+        continue
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        # words not found in embedding index will be all-zeros.
+        embedding_matrix[i] = embedding_vector
+
+# load pre-trained word embeddings into an Embedding layer
+# note that we set trainable = False so as to keep the embeddings fixed
+embedding_layer = Embedding(num_words,
+                            EMBEDDING_DIM,
+                            embeddings_initializer=Constant(embedding_matrix),
+                            input_length=MAX_SEQUENCE_LENGTH,
+                            trainable=False)
+
+print('Training model.')
+
+# train a 1D convnet with global maxpooling
+sequence_input = Input(shape=(MAX_SEQUENCE_LENGTH,), dtype='int32')
+embedded_sequences = embedding_layer(sequence_input)
+x = Conv1D(128, 5, activation='relu')(embedded_sequences)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = MaxPooling1D(5)(x)
+x = Conv1D(128, 5, activation='relu')(x)
+x = GlobalMaxPooling1D()(x)
+x = Dense(128, activation='relu')(x)
+preds = Dense(len(labels_index), activation='softmax')(x)
+
+model = Model(sequence_input, preds)
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=['acc'])
+
+
+
+history = model.fit(x_train, y_train,
+          batch_size=32,
+          epochs=1,
+          validation_data=(x_val, y_val))
