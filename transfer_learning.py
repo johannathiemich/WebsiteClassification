d2c64b0863f47f0249d3d6a6a75ac0d04c7c0e2a
Alejandro Robles <alejandro.robles@thetradedesk.com>
2019-05-06 20:27:52 -0700

    Checking git ignore command

---
+++ transfer_learning.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import numpy as np
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils import to_categorical
+from keras.layers import Dense, Input, GlobalMaxPooling1D
+from keras.layers import Conv1D, MaxPooling1D, Embedding
+from keras.models import Model
+from keras.initializers import Constant
+
+
+print('Indexing word vectors.')
+
+embeddings_index = {}
+with open(os.path.join(GLOVE_DIR, 'glove.6B.100d.txt')) as f:
+    for line in f:
+        word, coefs = line.split(maxsplit=1)
+        coefs = np.fromstring(coefs, 'f', sep=' ')
+        embeddings_index[word] = coefs
